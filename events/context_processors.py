5edf87690388d99be68861f4504e6d1858b9e50e
Gilbert J. M. Forkel <gilbert@erlangen.ccc.de>
2024-04-17 17:52:11 +0200

    Added option to disable display of logo on page head

--- events/context_processors.py
+++ events/context_processors.py
@@ -6,6 +6,11 @@ def site(request):
     d["EMAIL_SITE_URL"] = settings.EMAIL_SITE_URL
 
     try:
+        d["LOGO_ENABLE"] = settings.LOGO_ENABLE
+    except:
+        d["LOGO_ENABLE"] = False
+    
+    try:
         d["LOGO_URL"] = settings.LOGO_URL
     except:
         d["LOGO_URL"] = "/"
