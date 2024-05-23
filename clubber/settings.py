fee5b484ecadbaf6af75c82bc46b16dae1525880
Gilbert J. M. Forkel <gilbert@erlangen.ccc.de>
2024-05-23 16:18:59 +0200

    .

--- clubber/settings.py
+++ clubber/settings.py
@@ -186,11 +186,14 @@ DJANGO_FLATPICKR = {
 STATIC_ROOT = "static_ext"
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
